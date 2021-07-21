c3d896ee16ff5348a3099beff6d827e6fee5d903
Stephen Lane-Walsh <sdl.slane@gmail.com>
2021-07-21 12:07:03 -0400

    WIP Adding Graphics Driver

--- Engine/Private/GraphicsDriver.cpp
+++ Engine/Private/GraphicsDriver.cpp
@@ -8,13 +8,73 @@ namespace noon {
 NOON_API
 GraphicsDriver::GraphicsDriver()
 {
+    InitWindow();
+    InitInstance();
+    InitSurface();
+    InitLogicalDevice();
+    InitAllocator();
+    InitSwapChain();
+}
+
+NOON_API
+GraphicsDriver::~GraphicsDriver()
+{
+    TermSwapChain();
+    TermAllocator();
+    TermLogicalDevice();
+    TermSurface();
+    TermInstance();
+    TermWindow();
+}
+
+void GraphicsDriver::ProcessEvents()
+{
+    SDL_Event event;
+    while (SDL_PollEvent(&event)) {
+        if (event.type == SDL_QUIT) {
+            Application::GetInstance()->Stop();
+        }
+    }
+}
+
+void GraphicsDriver::Render()
+{
+}
+
+bool GraphicsDriver::IsDeviceSuitable(const VkPhysicalDevice device)
+{
+
+}
+
+std::vector<const char *> GraphicsDriver::GetRequiredLayers()
+{
+
+}
+
+std::vector<const char *> GraphicsDriver::GetRequiredDeviceExtensions()
+{
+
+}
+
+std::vector<const char *> GraphicsDriver::GetRequiredInstanceExtensions()
+{
+
+}
+
+void GraphicsDriver::GetPhysicalDevice()
+{
+
+}
+
+void GraphicsDriver::InitWindow()
+{
     if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
         throw Exception("SDL_Init failed, {}", SDL_GetError());
     }
 
     SDL_version version;
     SDL_GetVersion(&version);
-    Log(NOON_ANCHOR, "SDL Version: {}.{}.{}", 
+    Log(NOON_ANCHOR, "SDL {}.{}.{}", 
         (int)version.major, (int)version.minor, (int)version.patch);
 
     _sdlWindow = SDL_CreateWindow(
@@ -30,8 +90,7 @@ GraphicsDriver::GraphicsDriver()
     }
 }
 
-NOON_API
-GraphicsDriver::~GraphicsDriver()
+void GraphicsDriver::TermWindow()
 {
     if (_sdlWindow) {
         SDL_DestroyWindow(_sdlWindow);
@@ -40,19 +99,154 @@ GraphicsDriver::~GraphicsDriver()
     SDL_Quit();
 }
 
-void GraphicsDriver::ProcessEvents()
+void GraphicsDriver::InitInstance()
 {
-    SDL_Event event;
-    while (SDL_PollEvent(&event)) {
-        if (event.type == SDL_QUIT) {
-            Application::GetInstance()->Stop();
-        }
+    VkResult vkResult;
+
+    // Load Vulkan library and lookup core function addresses
+    int vkVersion = gladLoaderLoadVulkan(nullptr, nullptr, nullptr);
+    if (vkVersion == 0) {
+        throw Exception("gladLoaderLoadVulkan() failed");
+    }
+
+    Log(NOON_ANCHOR, "Vulkan {}.{}", GLAD_VERSION_MAJOR(vkVersion), GLAD_VERSION_MINOR(vkVersion));
+
+    // 
+    uint32_t availableLayerCount = 0;
+    vkEnumerateInstanceLayerProperties(&availableLayerCount, nullptr);
+    if (availableLayerCount == 0) {
+        throw Exception("vkEnumerateInstanceLayerProperties() failed, no available layers");
+    }
+
+    std::vector<VkLayerProperties> availableLayerList(availableLayerCount);
+    vkResult = vkEnumerateInstanceLayerProperties(&availableLayerCount, availableLayerList.data());
+    if (vkResult != VK_SUCCESS) {
+        throw Exception("vkEnumerateInstanceLayerProperties() failed, {}", VkResultToString(vkResult));
+    }
+
+    Log(NOON_ANCHOR, "Available Vulkan Layers:");
+    for (const auto& layer : availableLayerList) {
+        Log(NOON_ANCHOR, "\t{}: {}");
     }
 }
 
-void GraphicsDriver::Render()
+void GraphicsDriver::TermInstance()
 {
 
+}
+
+void GraphicsDriver::InitSurface()
+{
+
+}
+
+void GraphicsDriver::TermSurface()
+{
+
+}
+
+void GraphicsDriver::InitPhysicalDevice()
+{
+
+}
+
+void GraphicsDriver::InitLogicalDevice()
+{
+
+}
+
+void GraphicsDriver::TermLogicalDevice()
+{
+
+}
+
+void GraphicsDriver::InitAllocator()
+{
+
+}
+
+void GraphicsDriver::TermAllocator()
+{
+
+}
+
+void GraphicsDriver::InitSwapChain()
+{
+
+}
+
+void GraphicsDriver::TermSwapChain()
+{
+
+}
+
+void GraphicsDriver::ResetSwapChain()
+{
+
+}
+
+void GraphicsDriver::InitRenderPass()
+{
+
+}
+
+void GraphicsDriver::TermRenderPass()
+{
+
+}
+
+string VkResultToString(VkResult vkResult)
+{
+    switch (vkResult) {
+        case VK_SUCCESS:
+            return "VK_SUCCESS";
+        case VK_NOT_READY:
+            return "VK_NOT_READY";
+        case VK_TIMEOUT:
+            return "VK_TIMEOUT";
+        case VK_EVENT_SET:
+            return "VK_EVENT_SET";
+        case VK_EVENT_RESET:
+            return "VK_EVENT_RESET";
+        case VK_INCOMPLETE:
+            return "VK_INCOMPLETE";
+        case VK_ERROR_OUT_OF_HOST_MEMORY:
+            return "VK_ERROR_OUT_OF_HOST_MEMORY";
+        case VK_ERROR_OUT_OF_DEVICE_MEMORY:
+            return "VK_ERROR_OUT_OF_DEVICE_MEMORY";
+        case VK_ERROR_INITIALIZATION_FAILED:
+            return "VK_ERROR_INITIALIZATION_FAILED";
+        case VK_ERROR_DEVICE_LOST:
+            return "VK_ERROR_DEVICE_LOST";
+        case VK_ERROR_MEMORY_MAP_FAILED:
+            return "VK_ERROR_MEMORY_MAP_FAILED";
+        case VK_ERROR_LAYER_NOT_PRESENT:
+            return "VK_ERROR_LAYER_NOT_PRESENT";
+        case VK_ERROR_EXTENSION_NOT_PRESENT:
+            return "VK_ERROR_EXTENSION_NOT_PRESENT";
+        case VK_ERROR_FEATURE_NOT_PRESENT:
+            return "VK_ERROR_FEATURE_NOT_PRESENT";
+        case VK_ERROR_INCOMPATIBLE_DRIVER:
+            return "VK_ERROR_INCOMPATIBLE_DRIVER";
+        case VK_ERROR_TOO_MANY_OBJECTS:
+            return "VK_ERROR_TOO_MANY_OBJECTS";
+        case VK_ERROR_FORMAT_NOT_SUPPORTED:
+            return "VK_ERROR_FORMAT_NOT_SUPPORTED";
+        case VK_ERROR_FRAGMENTED_POOL:
+            return "VK_ERROR_FRAGMENTED_POOL";
+        case VK_ERROR_UNKNOWN:
+            return "VK_ERROR_UNKNOWN";
+        case VK_ERROR_OUT_OF_POOL_MEMORY:
+            return "VK_ERROR_OUT_OF_POOL_MEMORY";
+        case VK_ERROR_INVALID_EXTERNAL_HANDLE:
+            return "VK_ERROR_INVALID_EXTERNAL_HANDLE";
+        case VK_ERROR_FRAGMENTATION:
+            return "VK_ERROR_FRAGMENTATION";
+        case VK_ERROR_INVALID_OPAQUE_CAPTURE_ADDRESS:
+            return "VK_ERROR_INVALID_OPAQUE_CAPTURE_ADDRESS";
+    }
+
+    return "";
 }
 
 } // namespace noon
--- Engine/Public/Noon/GraphicsDriver.hpp
+++ Engine/Public/Noon/GraphicsDriver.hpp
@@ -4,6 +4,9 @@
 #include <Noon/Config.hpp>
 
 #include <SDL.h>
+#include <glad/vulkan.h>
+
+#include <vector>
 
 namespace noon {
 
@@ -21,12 +24,72 @@ public:
 
 private:
 
+    bool IsDeviceSuitable(const VkPhysicalDevice device);
+
+    std::vector<const char *> GetRequiredLayers();
+
+    std::vector<const char *> GetRequiredDeviceExtensions();
+
+    std::vector<const char *> GetRequiredInstanceExtensions();
+
+    void GetPhysicalDevice();
+
+    void InitWindow();
+
+    void TermWindow();
+
+    void InitInstance();
+
+    void TermInstance();
+
+    void InitSurface();
+
+    void TermSurface();
+
+    void InitLogicalDevice();
+
+    void TermLogicalDevice();
+
+    void InitAllocator();
+
+    void TermAllocator();
+
+    void InitSwapChain();
+
+    void TermSwapChain();
+
+    void ResetSwapChain();
+
+    void InitRenderPass();
+
+    void TermRenderPass();
+
     static GraphicsDriver * _Instance;
 
     SDL_Window * _sdlWindow;
 
+    std::unordered_map<string, VkLayerProperties> _vkAvailableLayers;
+
+    std::unordered_map<string, VkExtensionProperties> _vkAvailableInstanceExtensions;
+
+    std::unordered_map<string, VkExtensionProperties> _vkAvailableDeviceExtensions;
+    
+    VkInstance _vkInstance = nullptr;
+
+    VkDebugUtilsMessengerEXT _vkDebugMessenger = nullptr;
+
+    VkSurfaceKHR _vkSurface = nullptr;
+
+    VkPhysicalDeviceProperties _vkPhysicalDeviceProperties;
+    
+    VkPhysicalDeviceFeatures _vkPhysicalDeviceFeatures;
+
+    VkPhysicalDevice _vkPhysicalDevice = nullptr;
+
 
 }; // class GraphicsDriver
+
+string VkResultToString(VkResult vkResult);
 
 } // namespace noon
 
